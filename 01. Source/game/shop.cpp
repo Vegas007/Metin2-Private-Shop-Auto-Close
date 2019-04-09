6dea2e1c5aeba72628d36b7545ad78d0511627d8
Vegas007 <vegas_project1337@yahoo.com>
2019-04-09 15:46:58 +0300

    Remove count items + added support for c++11

--- 01. Source/game/shop.cpp
+++ 01. Source/game/shop.cpp
@@ -15,17 +15,29 @@ void CShop::SetPCShop(LPCHARACTER ch)
 	m_pkPC = ch;
 }
 //2.2) Add after:
+//c++98
 #ifdef ENABLE_PRIVATE_SHOP_AUTO_CLOSE
 bool CShop::IsSoldOut()
 {
-	BYTE bItemCount = 0;
 	for (size_t i = 0; i < m_itemVector.size() && i < SHOP_HOST_ITEM_MAX_NUM; ++i)
 	{
 		const SHOP_ITEM & r_item = m_itemVector[i];
 		if (r_item.pkItem)
-			++bItemCount;
+			return false;
 	}
-	
-	return (bItemCount == 0);
+	return true;
+}
+#endif
+
+//c++11
+#ifdef ENABLE_SHOP_AUTO_CLOSE
+bool CShop::IsSoldOut()
+{
+	for (auto & r_item : m_itemVector)
+	{
+		if (r_item.pkItem)
+			return false;
+	}
+	return true;
 }
 #endif
